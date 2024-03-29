c1bff713675ef60f14cc1c59dd67d91c16470a14
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-29 20:37:00 +0100

    Add Particles component and update background gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Particles from "./components/Particles";
 import RocketSVG from "./components/RocketSVG";
 
 export default function Home() {
@@ -5,9 +6,10 @@ export default function Home() {
     <div className="h-full grid grid-cols-[minmax(0,1fr),50vw] overflow-hidden">
       <div></div>
       <div className="overflow-hidden relative">
-        <div className="bg-gradient-to-tr from-sky-500 to-blue-700 h-full -skew-x-12 translate-x-24"></div>
+        <div className="bg-gradient-to-tr from-sky-500 via-blue-700 to-blue-950 h-full -skew-x-12 translate-x-24"></div>
 
-        <RocketSVG className="absolute top-1/2 left-1/2 h-1/2" />
+        <RocketSVG className="absolute top-1/2 left-1/2 h-1/2 z-20" />
+        <Particles />
       </div>
     </div>
   );
