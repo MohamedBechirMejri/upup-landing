d7f3436f72cb8b31dabc437cb3a47bf8cfea2eca
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-29 19:55:16 +0100

    Refactor RocketSVG position

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="overflow-hidden relative">
         <div className="bg-gradient-to-tr from-sky-500 to-blue-700 h-full -skew-x-12 translate-x-24"></div>
 
-        <RocketSVG className="absolute top-1/2 left-1/2 h-1/2 -translate-x-[37%] -translate-y-1/2" />
+        <RocketSVG className="absolute top-1/2 left-1/2 h-1/2" />
       </div>
     </div>
   );
