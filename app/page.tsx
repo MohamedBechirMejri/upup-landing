e5c0ff4c34f0b33c1f6e6296877b0173f53f3c12
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-29 20:47:12 +0100

    Refactor page layout and add gradient background

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,16 @@ export default function Home() {
   return (
     <div className="h-full grid grid-cols-[minmax(0,1fr),50vw] overflow-hidden">
       <div></div>
-      <div className="overflow-hidden relative">
-        <div className="bg-gradient-to-tr from-sky-500 via-blue-700 to-blue-950 h-full -skew-x-12 translate-x-24"></div>
+      <div className="overflow-hidden relative bg-gradient-to-tr from-sky-500 via-blue-700 to-blue-950 ">
+        <div
+          className="size-0 "
+          style={{
+            borderLeft: "20rem solid transparent",
+            borderRight: "0 solid transparent",
+            borderBottom: "100vh solid white",
+            transform: 'rotateX(180deg) rotateY(180deg)',
+          }}
+        ></div>
 
         <RocketSVG className="absolute top-1/2 left-1/2 h-1/2 z-20" />
         <Particles />
