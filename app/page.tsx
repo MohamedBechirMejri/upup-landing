41c80ef39e553c9ecf0d7b7ec99cc6711ede2d50
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-29 18:13:58 +0100

    Refactor grid layout and add gradient background

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,10 @@
-// "use client";
-
-// import styled, { css } from "styled-components";
-// import Footer from "./components/Footer";
-// import Intro from "./components/Intro";
-// const Container = styled.div`
-//   height: 100vh;
-//   overflow: hidden;
-//   position: relative;
-// `;
-
-// const Shape = css`
-//   width: 100%;
-//   height: 100%;
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   z-index: -1;
-// `;
-
-// const IntoShape = styled.div`
-//   ${Shape}
-//   clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
-//   background: linear-gradient(to right, #62cff4, #2c67f2);
-// `;
-
-// const FeatureShape = styled.div`
-//   ${Shape}
-//   clip-path: polygon(0 0, 55% 0%, 33% 100%, 0 100%);
-//   background-color: pink;
-// `;
-
-// const ServiceShape = styled.div`
-//   ${Shape}
-//   clip-path: polygon(0 0, 33% 0%, 33% 100%, 0 100%);
-//   background-color: #f88497;
-// `;
-
-// const PriceShape = styled.div`
-//   ${Shape}
-//   clip-path: polygon(33% 0, 100% 0%, 100% 100%, 67% 100%);
-//   background-color: crimson;
-// `;
-
-// const App = () => {
-//   const smallScreen = false;
-//   return (
-//     <>
-//       <Container>
-//         <Intro />
-//         <IntoShape />
-//       </Container>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default App;
-
 export default function Home() {
   return (
-    <div className="h-full grid grid-cols-[minmax(0,1fr),35vw]">
+    <div className="h-full grid grid-cols-[minmax(0,1fr),50vw] overflow-hidden">
       <div></div>
-      <div className="bg-sky-500 h-full"></div>
+      <div className="overflow-hidden">
+        <div className="bg-gradient-to-tr from-sky-500 to-blue-700 h-full -skew-x-12 translate-x-24"></div>
+      </div>
     </div>
   );
 }
