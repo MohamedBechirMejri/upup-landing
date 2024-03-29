c4c7c417a44be03880f17b7ca1eadc6d7c736bb7
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2024-03-29 17:46:12 +0100

    remove boilerplate code

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,62 @@
-"use client";
+// "use client";
 
-import React, { createContext } from "react";
-import styled, { css } from "styled-components";
-import Footer from "./components/Footer";
-import Intro from "./components/Intro";
-const Container = styled.div`
-  height: 100vh;
-  overflow: hidden;
-  position: relative;
-`;
+// import styled, { css } from "styled-components";
+// import Footer from "./components/Footer";
+// import Intro from "./components/Intro";
+// const Container = styled.div`
+//   height: 100vh;
+//   overflow: hidden;
+//   position: relative;
+// `;
 
-const Shape = css`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -1;
-`;
+// const Shape = css`
+//   width: 100%;
+//   height: 100%;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   z-index: -1;
+// `;
 
-const IntoShape = styled.div`
-  ${Shape}
-  clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
-  background: linear-gradient(to right, #62cff4, #2c67f2);
-`;
+// const IntoShape = styled.div`
+//   ${Shape}
+//   clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
+//   background: linear-gradient(to right, #62cff4, #2c67f2);
+// `;
 
-const FeatureShape = styled.div`
-  ${Shape}
-  clip-path: polygon(0 0, 55% 0%, 33% 100%, 0 100%);
-  background-color: pink;
-`;
+// const FeatureShape = styled.div`
+//   ${Shape}
+//   clip-path: polygon(0 0, 55% 0%, 33% 100%, 0 100%);
+//   background-color: pink;
+// `;
 
-const ServiceShape = styled.div`
-  ${Shape}
-  clip-path: polygon(0 0, 33% 0%, 33% 100%, 0 100%);
-  background-color: #f88497;
-`;
+// const ServiceShape = styled.div`
+//   ${Shape}
+//   clip-path: polygon(0 0, 33% 0%, 33% 100%, 0 100%);
+//   background-color: #f88497;
+// `;
 
-const PriceShape = styled.div`
-  ${Shape}
-  clip-path: polygon(33% 0, 100% 0%, 100% 100%, 67% 100%);
-  background-color: crimson;
-`;
+// const PriceShape = styled.div`
+//   ${Shape}
+//   clip-path: polygon(33% 0, 100% 0%, 100% 100%, 67% 100%);
+//   background-color: crimson;
+// `;
 
-const App = () => {
-  const smallScreen = false;
-  return (
-    <>
-      <Container>
-        <Intro />
-        <IntoShape />
-      </Container>
-      <Footer />
-    </>
-  );
-};
+// const App = () => {
+//   const smallScreen = false;
+//   return (
+//     <>
+//       <Container>
+//         <Intro />
+//         <IntoShape />
+//       </Container>
+//       <Footer />
+//     </>
+//   );
+// };
 
-export default App;
+// export default App;
+
+export default function Home() {
+  return <div></div>;
+}
